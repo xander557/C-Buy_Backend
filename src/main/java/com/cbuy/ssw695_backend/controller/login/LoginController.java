93ffede04712547e32f34d3e357d2505a18461a0
xander557 <yikanwang557@gmail.com>
2022-10-23 18:30:47 -0700

    not finished user loging module

--- src/main/java/com/cbuy/ssw695_backend/controller/login/LoginController.java
+++ src/main/java/com/cbuy/ssw695_backend/controller/login/LoginController.java
@@ -1,14 +1,54 @@
 package com.cbuy.ssw695_backend.controller.login;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/user/")
 public class LoginController {
 
-    @RequestMapping("login/")
-    public String getLogInfo(){
-        return ("Hello World Hello World");
+    final
+    UserMapper userMapper;
+
+    public LoginController(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping("/user/all/")
+    public List<User> getAll() {
+        return userMapper.selectList(null);
+    }
+
+    @GetMapping("/user/{userId}/")
+    public User getuser(@PathVariable int userId) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id", userId);
+        return userMapper.selectOne(queryWrapper);
+    }
+
+    @GetMapping("/user/add/{userId}/{username}/{password}/")
+    public String addUser(
+            @PathVariable int userId,
+            @PathVariable String username,
+            @PathVariable String password) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User(userId, username, encodedPassword);
+        userMapper.insert(user);
+        return "Add User Successfully";
+    }
+
+    @GetMapping("/user/delete/{userId}/")
+    public String deleteUser(@PathVariable int userId) {
+        userMapper.deleteById(userId);
+        return "Delete User Successfully";
     }
 }
